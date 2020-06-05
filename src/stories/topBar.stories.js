9c73fdef6608bb091e09004db2c4d37784326533
ChenyoisRita <cyandwsf@gmail.com>
2020-06-05 15:15:36 -0400

    initialize the Top Bar

---
+++ src/stories/topBar.stories.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import TopBar from '../topBar';
+
+export default {
+    component: TopBar,
+    title: 'TopBar',
+  };
+
+export const Default = () => <TopBar />;
+// ReactDOM.render(<TopBar />, document.querySelector('#root'));
